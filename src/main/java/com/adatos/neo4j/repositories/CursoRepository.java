2f2a7451bd506bab6d26cf66ab51693acd9a2d7a
alanv <alanvallve@gmail.com>
2018-03-14 18:58:03 +0100

    sdgsdgs

--- src/main/java/com/adatos/neo4j/repositories/CursoRepository.java
+++ src/main/java/com/adatos/neo4j/repositories/CursoRepository.java
@@ -3,9 +3,10 @@ package com.adatos.neo4j.repositories;
 import com.adatos.neo4j.domain.Curso;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Service;
 
+@Service
 @RepositoryRestResource(collectionResourceRel = "movies", path = "movies")
 public interface CursoRepository extends Neo4jRepository<Curso, Long> {
-
 
 }
